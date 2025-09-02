bb2dbcc0f35fa3b72ba8f68a1435e4c3bcb35b6e
Valentina Efimov <valentinaefimov0811@gmail.com>
2025-09-02 18:53:09 -0300

    Resolución Ejercicio4 [Efimov Valentina]

--- src/ar/edu/unju/escmi/tp2/ejercicio4/Ejercicio4.java
+++ src/ar/edu/unju/escmi/tp2/ejercicio4/Ejercicio4.java
@@ -7,19 +7,17 @@ public class Ejercicio4 {
         System.out.println(resultado);
 
         /*
-        
-         La clase Math de Java forma parte del paquete java.lang, por lo cual, está disponible 
-        automáticamente en cualquier programa Java y no necesita ser importada explícitamente.
-        Proporciona métodos estáticos para realizar operaciones matemáticas y trigonométricas.
-         Al ser métodos estáticos, se pueden usar directamente llamando a la clase 
-        sin necesidad de crear un objeto de Math. 
+            La clase Math de Java forma parte del paquete java.lang, por lo cual, está disponible 
+            automáticamente en cualquier programa Java y no necesita ser importada explícitamente.
+            Proporciona métodos estáticos para realizar operaciones matemáticas y trigonométricas.
+            Al ser métodos estáticos, se pueden usar directamente llamando a la clase 
+            sin necesidad de crear un objeto de Math. 
 
-         Math.pow() proporciona una forma estandarizada de realizar exponenciaciones. 
-        Este método toma dos argumentos: la base (el número que se eleva a una potencia) 
-        y el exponente (exponente al que se eleva el número base), ambos de tipo double, 
-        y devuelve un resultado de tipo double.
-         Math.pow (double base , double exponente)
-
+            Math.pow() proporciona una forma estandarizada de realizar exponenciaciones. 
+            Este método toma dos argumentos: la base (el número que se eleva a una potencia) 
+            y el exponente (exponente al que se eleva el número base), ambos de tipo double, 
+            y devuelve un resultado de tipo double.
+            Math.pow (double base , double exponente)
         */
     }
 }
