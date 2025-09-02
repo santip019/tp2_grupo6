86260097b104faa04d8a33ecc67bf5064003ede2
Antonella Guadalupe Cruz <antoguadalupecruz@gmail.com>
2025-09-01 22:11:43 -0300

    Se Realiza el punto 2 [ Cruz Antonella ]

--- src/ar/edu/unju/escmi/tp2/ejercicio2/Ejercicio2.java
+++ src/ar/edu/unju/escmi/tp2/ejercicio2/Ejercicio2.java
@@ -2,21 +2,21 @@ package ar.edu.unju.escmi.tp2.ejercicio2;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-            String nombre1 = "María Díaz";
-            String nombre2 = "Juan Gonzalez";
-            String nombre3 = "Julieta Velazquez";
-            String nombre4 = "Silvia Juarez";
+        String nombre1 = "María Díaz";
+        String nombre2 = "Juan Gonzalez";
+        String nombre3 = "Julieta Velazquez";
+        String nombre4 = "Silvia Juarez";
 
-            int doc1 = 45200145;
-            int doc2 = 44120789;
-            int doc3 = 42001796;
-            int doc4 = 43100789;
+        int doc1 = 45200145;
+        int doc2 = 44120789;
+        int doc3 = 42001796;
+        int doc4 = 43100789;
 
-            System.out.println(
-                    nombre1 + " - " + doc1 + "\n" +
-                    nombre2 + " - " + doc2 + "\n" +
-                    nombre3 + " - " + doc3 + "\n" +
-                    nombre4 + " - " + doc4
-                );
+        System.out.println(
+            nombre1 + " - " + doc1 + "\n" +
+            nombre2 + " - " + doc2 + "\n" +
+            nombre3 + " - " + doc3 + "\n" +
+            nombre4 + " - " + doc4
+         );
     }
 }
