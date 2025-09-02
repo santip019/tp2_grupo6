a4ba490f666592035af6e2205ef673951a14dfa4
Valentina Efimov <valentinaefimov0811@gmail.com>
2025-09-01 23:43:02 -0300

    Resolución Ejercicio16 [Efimov Valentina]

--- src/ar/edu/unju/escmi/tp2/ejercicio11/Ejercicio11.java
+++ src/ar/edu/unju/escmi/tp2/ejercicio11/Ejercicio11.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Ejercicio11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num;
+        
         for (byte i=1; i<=5; i++)
         {
             System.out.println("Ingrese el "+i+"° número: ");
-            int num = sc.nextInt();
+            num = sc.nextInt();
             
             if (num%2 == 0)
              System.out.println("El número "+num+" es par");
